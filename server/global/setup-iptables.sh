4d6e402265ab65b8d4279ca5762405670c67eec3
Brayan Capetillo <brayancapetoccc@gmail.com>
2025-06-03 21:32:43 -0600

    :wrench: chore(iptables): add init script to configure basic iptables rules

---
+++ server/global/setup-iptables.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# +========= CLEAR EXISTING RULES ==========+
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+
+# +=========== DEFAULT POLICIES ============+
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# +============= BASIC RULES ==============+
+iptables -A INPUT -i lo -j ACCEPT                                   # Loopback
+iptables -A INPUT -p tcp --dport 3128 -j ACCEPT                     # Squid
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT                       # Nginx (general)
+iptables -A INPUT -p udp --dport 53 -j ACCEPT                       # DNS UDP
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT                       # DNS TCP
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT        # Allow ping (ICMP echo-request)
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT    # Established connections
+
+# +============== DEBUGGING ===============+
+iptables -L -v
+iptables -S
+
+# +=========== RUN SUPERVISORD ============+
+exec "$@"
