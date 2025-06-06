18ec75a88b4e2f7d797edbe7a5fc9289a241f10e
Brayan Capetillo <brayancapetoccc@gmail.com>
2025-06-05 23:54:12 -0600

    :sparkles: feat(iptables): add rule to block access to port 21 from specific IP

--- server/global/setup-iptables.sh
+++ server/global/setup-iptables.sh
@@ -17,6 +17,8 @@ iptables -P OUTPUT ACCEPT
 # 1. ⛔ Deny access to port 80 (HTTP) from specific IP
 iptables -I INPUT -p tcp --dport 80 -s 10.89.0.10 -j REJECT --reject-with tcp-reset
 
+# 2. Block access to port 21 (FTP) from specific IP
+iptables -I INPUT -p tcp --dport 21 -s 10.89.0.20 -j REJECT --reject-with tcp-reset
 
 
 # +============= BASIC RULES ==============+
@@ -25,6 +27,7 @@ iptables -A INPUT -p tcp --dport 3128 -j ACCEPT                     # Squid
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT                       # Nginx (general)
 iptables -A INPUT -p udp --dport 53 -j ACCEPT                       # DNS UDP
 iptables -A INPUT -p tcp --dport 53 -j ACCEPT                       # DNS TCP
+iptables -A INPUT -p tcp --dport 21 -j ACCEPT                       # FTP port 21 allowed
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT        # Allow ping (ICMP echo-request)
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT    # Established connections
 
